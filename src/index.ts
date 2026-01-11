2bb6f023146ba4dd897f791c1c20011551891316
mikesaintsg <45922630+mikesaintsg@users.noreply.github.com>
2026-01-11 13:52:53 -0500

    Delete src directory

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Documenta - Browser PDF Editor
- * @module documenta
- */
-
-export * from './factories.js'
-export * from './helpers.js'
-export * from './constants.js'
-export type * from './types.js'
